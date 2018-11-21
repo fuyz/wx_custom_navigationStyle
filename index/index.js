3349f3c6492736b617027e65bc097fa9b6b5c2fa
fuyz <953958160@qq.com>
2018-11-21 11:19:17 +0800

    upload file

---
+++ index/index.js
@@ -0,0 +1,11 @@
+const navigationBarHeight = (getApp().statusBarHeight + 44) + 'px'
+
+Page({
+  data: {
+    navigationBarTitle: '自定义 Navigation Bar',
+    navigationBarHeight
+  },
+  onLoad: function () {
+    
+  },
+})
